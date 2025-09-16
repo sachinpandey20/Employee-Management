c90c46c7213a6e429cd8b056a472b7ed0a01f7db
Sachin Pandey <sachipandey19.sp@gmail.com>
2025-09-17 00:18:23 +0530

    Initial Commit

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>Mafe By: Sachin 2025</span>
+        </footer>
+
+    </div>
+  )
+}
+
+export default FooterComponent
